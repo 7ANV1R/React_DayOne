3df82f1e8f0a845459fb1e71ae3646fb7aa48b9f
7anv1r <ibnmizan7878@gmail.com>
2022-01-14 17:35:43 +0600

    kinda start commit

---
+++ src/SwapButton.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import ReplayIcon from '@mui/icons-material/Replay';
+import ClearIcon from '@mui/icons-material/Clear';
+import StarIcon from '@mui/icons-material/Star';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material';
+import './SwapButton.css'
+function SwapButton() {
+ return (
+  <div className='swap__button'> 
+   <IconButton className='swap__icon_box'>
+    
+    <ReplayIcon className='repeat__icon'/>
+    </IconButton>
+    
+    <IconButton className='swap__icon_box'>
+    
+   <ClearIcon className='cross__icon'/>
+   </IconButton>
+   <IconButton className='swap__icon_box'>
+    
+   <StarIcon className='star__icon'/>
+   </IconButton>
+   <IconButton className='swap__icon_box'> 
+    
+   <FavoriteIcon className='heart__icon'/>
+   </IconButton>
+   <IconButton className='swap__icon_box'> 
+    
+    <FlashOnIcon className='flash__icon'/>
+    </IconButton>
+  </div>
+ )
+}
+
+export default SwapButton
---
+++ src/TinderCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TinderCards.css'
+function TinderCards() {
+ return (
+  <div className='tinder__card'>
+   
+  </div>
+ )
+}
+
+export default TinderCards
