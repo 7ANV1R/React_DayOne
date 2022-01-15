38bf627eb2da5e77b9b204805ae6a1b1868ea56a
Tanvir <ibnmizan7878@gmail.com>
2022-01-16 04:11:42 +0600

    updated tinder card part

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TinderCard from 'react-tinder-card';
 import './TinderCards.css'
 function TinderCards() {
+ const [people, setPeople] = useState([
+  {
+   name: 'Hero Elon',
+   url: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg'
+  },
+  {
+   name: 'Hero Alom',
+   url: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Hero_alom.jpg'
+  }
+ ]);
+ 
+ const swiped = (direction, nameToDelete) => {
+  console.log("removing: " + nameToDelete);
+  // setLastDirection(direction);
+ };
+ 
+ const outOfFrame = (name) => {
+  console.log(name + " left the screen!");
+ };
  return (
+  
   <div className='tinder__card'>
+   
+   <div className='tinderCards_Container'>
+   {people.map(person => (
+    <TinderCard
+    className='swipe'
+    key={person.name}
+    preventSwipe={['up', 'down']}
+    onSwipe={(dir) => swiped(dir, person.name)}
+    onCardLeftScreen={() => outOfFrame(person.name)}
+    >
+     
+     <div style={{backgroundImage: `url(${person.url})`}} className='card' >
+     <h3>{person.name}</h3>
+     </div>
+     
+     
+    </TinderCard>
+    
+   ))}
+   </div>
+   
+   
    
   </div>
  )
