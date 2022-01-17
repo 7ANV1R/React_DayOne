c6d1aec70ad37fe07ab65d61958423b4ef752df0
Tanvir <ibnmizan7878@gmail.com>
2022-01-17 07:38:47 +0600

    updated format

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-
-import './App.css';
-import Header from './Header';
-import SwapButton from './SwapButton';
-import TinderCards from './TinderCards';
+import "./App.css";
+import Header from "./Header";
+import SwapButton from "./SwapButton";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <TinderCards/>
+      <TinderCards />
       <SwapButton />
-      
     </div>
   );
 }
