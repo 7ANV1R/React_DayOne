8e24aefcace970dfd23adf45a06dcfa3c93f21d6
Tanvir <ibnmizan7878@gmail.com>
2022-01-19 04:04:21 +0600

    update

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import axios from "./axios";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Hero Elon",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
-    },
-    {
-      name: "Hero Alom",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Hero_alom.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/card");
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing: " + nameToDelete);
@@ -33,7 +33,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
